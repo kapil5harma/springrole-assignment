215928ed3daddf16cc364d74e0644805cce63ed2
Kapil Sharma <kapilsharmajmps@gmail.com>
2019-04-05 13:21:02 +0530

    config to store api endpoints

---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+export const rootConfig = {
+  apiRoot: `https://springrole.com/api/v1/user/c4f4c607-e9c8-4acb-9893-93c541bddf35`
+};
+
+export const apiEndpoints = {
+  workExperience: `${rootConfig.apiRoot}/work-experience`,
+  skills: `${rootConfig.apiRoot}/profile/skills`,
+  education: `${rootConfig.apiRoot}/education`
+};
